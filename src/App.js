97e1a560f3e713bfc7ae5573d8f34abb8e2807dd
Julia Harper <julia.harper@myob.com>
2017-07-23 21:20:26 +1000

    add css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import List from './list';
 
+
 class App extends Component {
 
   render() {
@@ -9,6 +10,9 @@ class App extends Component {
         <header>
           <h1>Swarm Management App</h1>
         </header>
+        <img src="https://s-media-cache-ak0.pinimg.com/736x/19/e7/25/19e7251c6a0eec74ff7fd039f8c51518--unicorn-pictures-black-unicorn.jpg" />
+        <img src="https://i2.wp.com/www.tor.com/wp-content/uploads/2014/12/inflatable-cat-unicorn-horn.jpg?fit=640%2C%209999&crop=0%2C0%2C100%2C640px" />
+        <img src="https://s-media-cache-ak0.pinimg.com/736x/31/80/a3/3180a39b733032f7f191c904384a46f1--unicorn-funny-team-unicorn.jpg" />
         <List items={[]} heading="Swarm Items" textBoxLabel="Add New Swarm Item" />
         <List items={[]} heading="Annoyance Items" textBoxLabel="Add New Annoyance Item" />
       </div>
